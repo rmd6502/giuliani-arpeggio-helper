115c449862a8febe7e6cf53353510648dc9ff0a3
robwatsongtr <rwatso@gmail.com>
2021-03-24 11:26:40 -0700

    ver 1.0 finally

--- giuliani-app-client/src/components/mainpage-component.js
+++ giuliani-app-client/src/components/mainpage-component.js
@@ -9,8 +9,7 @@ const Study = props => {
   return (
     <tr key={props.studyID}>
       <td>{props.study.difficulty}</td>
-      <td>{props.study.studyNum}</td>
-      <td><img src={props.study.studyPath}></img></td>
+      <td><img src={props.study.studyPath} alt="Giuliani Study"></img></td>
     </tr>
   )
 }
@@ -29,6 +28,8 @@ export default class MainPage extends Component {
     this.onChangeStudyDifficultyLevel = this.onChangeStudyDifficultyLevel.bind(this);
     this.onChangeStudyResultLimit = this.onChangeStudyResultLimit.bind(this);
     this.onChangeStudyNoDifficulty = this.onChangeStudyNoDifficulty.bind(this);
+    this.fetchStudiesByDifficulty = this.fetchStudiesByDifficulty.bind(this);
+    this.fetchAllStudies = this.fetchAllStudies.bind(this);
     this.studyList = this.studyList.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
@@ -51,9 +52,7 @@ export default class MainPage extends Component {
     })
   }
 
-  onSubmit(e) {
-    e.preventDefault();
-  
+  fetchStudiesByDifficulty() {
     const diffLevel = this.state.studyDifficultyLevel;
     const resultLimit = this.state.studyResultLimit;
 
@@ -76,12 +75,44 @@ export default class MainPage extends Component {
     .catch( (error) => {
       console.log(error);
     })
+  }
 
+  fetchAllStudies() {
+    const resultLimit = this.state.studyResultLimit;
+
+    fetch('http://localhost:5000/get-randomized-studies-all?' + new URLSearchParams({
+        limit: resultLimit
+      }), {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+    })
+    .then( res => {
+      return res.json()
+    })
+    .then ( data => {
+      this.setState({ studies: data })
+    })
+    .catch( (error) => {
+      console.log(error);
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if( this.state.studyNoDifficulty === 'on' ) {
+      this.fetchAllStudies()
+    } else {
+      this.fetchStudiesByDifficulty()
+    }
+    
     // reset state for 'no difficulty level' checkbox
     this.setState({
       studyNoDifficulty: false
     })
-
   }
 
   studyList() {
@@ -148,7 +179,7 @@ export default class MainPage extends Component {
           <thead>
             <tr>
               <th>Level</th>
-              <th>Number</th>
+              
               <th>Study</th>
             </tr>
           </thead>
